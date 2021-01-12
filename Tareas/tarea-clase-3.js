ad97655b8ceec183a92347ba05264dfb73ebc8d8
Magali Pujol <magalipuuujol@gmail.com>
2021-01-12 17:20:44 -0300

    tarea clase 3

--- Tareas/tarea-clase-3.js
+++ Tareas/tarea-clase-3.js
@@ -7,9 +7,38 @@
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
 
+let userName = prompt("Enter your name")
+
+if (userName === "Magali"){
+    console.log ("Hello, my name is also Magali!")
+}
+else if (userName === "Isabel") {
+    console.log ("Hi Isabel, your name is the same as my mom's")
+}
+else {
+    console.log (`Hi ${userName}`)
+}
+
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
+
+let userAge = Number(prompt("Enter your age."))
+
+if (userAge === "20") {
+    console.log ("I'm also 20 years old.")
+}
+else if (userAge < "20") {
+    console.log ("You're younger than me.")
+}
+else if (userAge > "20") {
+    console.log ("You're older than me.")
+}
+else {
+    console.log (`${userAge} is not a valid value.`)
+}
+
+
 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
@@ -19,3 +48,23 @@
 // Si no tiene documento, no dejarlo entrar al bar.
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
+
+let userID = prompt ("Do you have ID?").toLowerCase();
+
+let userAge;
+if (userID === "no") {
+    console.log ("You cannot enter the bar.")
+}
+else if (userID === "yes") {
+    let userAge = prompt ("Enter your age.")
+    if (userAge >= "18"){
+        console.log ("You can enter the bar.")
+    }
+    else if (userAge < "18") {
+        console.log ("You cannot enter the bar.")
+    }
+}
+else {
+    console.log (`${userID} is not a valid answer.`)
+}
+
