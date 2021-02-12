1ce59911fbcd3da6eac75b069d39df511b3c7cd9
magalipujol <magalipuuujol@gmail.com>
2021-02-12 20:26:10 -0300

    update tarea clase 4

--- Tareas/tarea-clase-4-algoritmos.js
+++ Tareas/tarea-clase-4-algoritmos.js
@@ -1,53 +1,8 @@
 //Link original y créditos a https://gith>b.com/CodeGuppyPrograms/CodingChallenges (contiene las respuestas).
 
 /*
-Estos desafíos de coding están enfocados para los principiantes, por lo tanto 
-las soluciones fueron implementadas
-usando  elementos de programación simples y clásicos.
-*/
-
-/*
-
- 
-Desafío de programación #3: Imprimí la tabla de multiplicación del 7
- 7x0 = 0
- 7x1 = 7
- ...
- 7x9 = 63
-Desafío de programación #4: Imprimí todas las tablas de multiplicación del 1 al 9
- 1x0 = 0
- ...
- 1x9 = 9
- 2x0 = 0
- ...
- 2x9 = 18
- ...
- ...
- 9x9 = 81
-Desafío de programación #5: Calcula el resultado de la suma de los numeros del 1 al 10 en un array.
- [1,2,3,4,5,6,7,8,9,10]
- 1 + 2 + 3 + ... + 10 = ? //hacerlo con un array y un bucle
-Desafío de programación #6: Calcula 10! (10 factorial)
- 10 * 9 * 8 * ... * 1
-Desafío de programación #7: Calcula la suma de todos los números impares mayores que 10 y menores que 30
-Desafío de programación #8: Crea una función que convierta de Celsius a Fahrenheit
-Desafío de programación #9: Crea una función que convierta de Fahrenheit a Celsius
-Desafío de programación #10: Calcula la suma de todos los números en un array de números
-Desafío de programación #11: Calcula el promedio de todos los números en un array de números. (en una función)
-Desafío de programación #12: Crea una función que reciba un array de números y devuelva un array conteniendo solo los números positivos
-Desafío de programación #13: Find the maximum number in an array of numbers
-Desafío de programación #14: Imprimir los primeros 10 dígitos de Fibonacci sin recursión
-Fibonacci (lo buscan en google si no lo saben o no se acuerdan)
-El resultado debería ser: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-Desafío de programación #15: Crear una función que encuentre el número n de Fibonacci usando recursión
-Desafío de programación #16: Crear una función que dado un número retorne un Boolean (true/false) dependiendo si es primo o no.
-Desafío de programación #17: Calcular la suma de los dígitos positivos de un número entero positivo
-Ejemplo:
-   123 = 1 + 2 + 3 = 6
-   2 = 2
-   1234 = 1 + 2 + 3 + 4 = 10
 Desafío de programación #18: Imprimir los primeros 100 números primos
-Desafío de programación #19: Crear una función que retorne un array con los primeros números "n-primos" mayores que un número particular definido "alComienzo"
+guga: Desafío de programación #19: Crear una función que retorne un array con los primeros números "n-primos" mayores que un número particular definido "alComienzo"
 Ejemplo:
   Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7,11,13,17,19]
 Desafío de programación #20: Rotar un array hacia la izquierda una posición
@@ -59,7 +14,7 @@ Desafío de programación #21: Rotar un array a la derecha una posición
 Ejemplo:
   [2,3,4,1] debería quedar como [1,2,3,4]
   [2,3] debería quedar como [3,2]
-Desafío de programación #22: Invertir un array
+guga: Desafío de programación #22: Invertir un array
 Ejemplo:
   [1,2,3,4] debería quedar como [4,3,2,1]
   [6,5,4,3,2,1] debería quedar como [1,2,3,4,5,6]
@@ -91,7 +46,7 @@ Desafío de programación #30-a: Create a function that will add two positive nu
 Desafío de programación #30-b: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
 Desafío de programación #31-a. Create a function that will return the number of words in a text
 Desafío de programación #31-b. Create a function that will return the number of words in a text
-Desafío de programación #32. Create a function that will capitalize the first letter of each word in a text
+guga: Desafío de programación #32. Create a function that will capitalize the first letter of each word in a text
 Desafío de programación #33. Calculate the sum of numbers received in a comma delimited string
 Desafío de programación #34. Create a function that will return an array with words inside a text
 Desafío de programación #35. Create a function to convert a CSV text to a “bi-dimensional” array
@@ -99,12 +54,12 @@ Desafío de programación #36. Create a function that converts a string to an ar
 Desafío de programación #37. Create a function that will convert a string in an array containing the ASCII codes of each character
 Desafío de programación #38. Create a function that will convert an array containing ASCII codes in a string
 Desafío de programación #39. Implement the Caesar cypher
-Desafío de programación #40. Implement the bubble sort algorithm for an array of numbers
-Desafío de programación #41. Create a function to calculate the distance between two points defined by their x, y coordinates
+guga: Desafío de programación #40. Implement the bubble sort algorithm for an array of numbers
+guga: Desafío de programación #41. Create a function to calculate the distance between two points defined by their x, y coordinates
 Desafío de programación #42. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting
 Desafío de programación 43. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
 Desafío de programación #44. Create a function that will convert a string containing a binary number into a number
-Desafío de programación #45. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
+guga: Desafío de programación #45. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
 Desafío de programación #46-a. Find the maximum number in a jagged array of numbers or array of numbers
 Desafío de programación #46-b. Find the maximum number in a jagged array of numbers or array of numbers
 Desafío de programación #47. Deep copy a jagged array with numbers or other arrays in a new array
@@ -239,21 +194,55 @@ function findMaxNumber (array) {
 console.log(findMaxNumber(array))
 //Desafío de programación #14: Imprimir los primeros 10 dígitos de Fibonacci 
 //sin recursión
-*/
-function FibonacciSuccession (end){
+function FibonacciSuccession(end) {
   FibonacciArray = [0, 1];
-  for (i = 1; i < end - 1; i++){
+  for (i = 1; i < end - 1; i++) {
     FibonacciArray.push(FibonacciArray[i] + FibonacciArray[i - 1])
   }
   return FibonacciArray
 }
 
-console.log(FibonacciSuccession(10)) 
+console.log(FibonacciSuccession(10))
+
+function SuccessionFibonacci(end) {
+  let a = 0
+  let b = 1
+  console.log(a)
+  console.log(b)
+  for (i = 2; i < end; i++) {
+    let c = a + b
+    a = b
+    b = c
+    console.log(c)
+  }
+}
+
+SuccessionFibonacci(10)
+//Desafío de programación #15: Crear una función que encuentre el número n de Fibonacci usando recursión
+function nthFibonacci (number) {
+  if (number === 0) {
+    return 0
+  }
+  else if ( number === 1) {
+    return 1
+  }
+  else {
+    return nthFibonacci (number - 1) + nthFibonacci (number - 2)
+  }
+}
+
+console.log(nthFibonacci(6))
+//Desafío de programación #16: Crear una función que dado un número retorne un Boolean (true/false) 
+//dependiendo si es primo o no.
+function isPrime (number){
+  for (i = 2; i < number; i++){
+    if (number % i === 0){
+      return false
+    }
+  }
+  return true
+}
+*/
 
 
-//TODOOOOOOOOOOOOOOOOOOO
-function SuccessionFibonacci (end){
-  for (i = 1; i = end; i++){
-    
-}
-}
+//Desafío de programación #17: Calcular la suma de los dígitos positivos de un número entero positivo
