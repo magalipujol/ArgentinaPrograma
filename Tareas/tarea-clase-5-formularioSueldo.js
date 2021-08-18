d7267d54209095bad66714e1cfcbe4743daedc78
Magali Pujol <magalicpujol@gmail.com>
2021-08-18 10:27:24 -0300

    created form

---
+++ Tareas/tarea-clase-5-formularioSueldo.js
@@ -0,0 +1,5 @@
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", 
+// mostrar el salario mensual
+// en una caja de texto deshabilitada. 
+// --> <input type="text" disabled id="salario-mensual"/>
