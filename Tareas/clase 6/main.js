0e7acb8a15426534e31d14215bc4719a9eb01a69
magali <magalipuuujol@gmail.com>
2022-03-17 14:54:35 -0300

    tarea clase 6 (sin terminar)

--- Tareas/clase 6/main.js
+++ Tareas/clase 6/main.js
@@ -1,10 +1,15 @@
 const $cantidadIntegrantes = document.querySelector("#cantidadIntegrantes");
 const $siguienteBtn = document.querySelector("#siguienteBtn");
 const $firstForm = document.querySelector("#firstQuestion");
+const $alertEmptyForm = document.querySelector('#formVacio')
 
 let cantidadIntegrantes;
 
 $siguienteBtn.onclick = function () {
+    if($cantidadIntegrantes.value == false){
+        $alertEmptyForm.innerHTML = 'Debe ingresar una cantidad'
+        return
+    }
   cantidadIntegrantes = $cantidadIntegrantes.value;
   console.log(cantidadIntegrantes);
   $firstForm.classList.add("hidden");
@@ -12,14 +17,17 @@ $siguienteBtn.onclick = function () {
 };
 
 const formLabel = document.createElement("label");
+formLabel.setAttribute("for", "edad");
+formLabel.innerHTML = "Edad"
 const formInput = document.createElement("input")
-formLabel.setAttribute("for", "nombre");
-formInput.innerHTML = "Nombre"
-formInput.setAttribute("")
+formInput.setAttribute("type", "number")
+formInput.classList.add("edadInput")
+
 
 
 function crearSegundoForm() {
-  for (let i = 0; i < cantidadIntegrantes; i++) {
+    for (let i = 0; i < cantidadIntegrantes; i++) {
       document.querySelector("body").appendChild(formLabel);
+      document.querySelector("body").appendChild(formInput);
   }
 }
